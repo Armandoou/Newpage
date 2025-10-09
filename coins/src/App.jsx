bf0281f515212028b31827d77181e592948c6789
CIAC <CIAC@CIAC2-006.academic.mixcoac.upmx.mx>
2025-10-09 13:00:44 -0600

    hola

---
+++ coins/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+import Layout from "./Components/layout";
+
+function App() {
+
+
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  )
+}
+
+export default App
